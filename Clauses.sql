e08dfa229b5e97b12402701b68a3d8a3c84d52bc
Akhilesh DB wala <akhileshsh029@outlook.com>
2017-09-21 12:39:21 +0530

    Clauses
    We can add these to a query for adding additional
    options like filtering the records, sorting records and grouping the records with in a
    table. These clauses contains the following clauses are,

---
+++ Clauses.sql
@@ -0,0 +1,6 @@
+ -- clauses  where , orderby , Top n , Group By , Having 
+ 
+ -- 1) by default data repreasent in acending order.
+ 
+      select * from Employee order by Eno
+      select Top 3 * from Employee order by Eno
