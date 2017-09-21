a8ec242b478200baa1da60bace8e3f2b86472716
Akhilesh DB wala <akhileshsh029@outlook.com>
2017-09-21 13:59:03 +0530

    Group BY
    Group by clause will use for to arrange similar data into groups.
    when we apply group by clause in the query then we use group functions like
    count(),sum(),max(),min(),avg().
    If we use group by clause in the query, first the data in the table will be divided
    into different groups based on the columns and then execute the group function on
    each group to get the result.

--- Clauses.sql
+++ Clauses.sql
@@ -1,6 +1,14 @@
  -- clauses  where , orderby , Top n , Group By , Having 
  
- -- 1) by default data repreasent in acending order.
+ -- 1) Order by default data repreasent in acending order.
  
       select * from Employee order by Eno
       select Top 3 * from Employee order by Eno
+      
+ --2)  GROUP BY: Group by clause will use for to arrange similar data into groups.
+               --when we apply group by clause in the query then we use group functions like
+               --count(),sum(),max(),min(),avg().
+          
+          SELECT COUNT (*) FROM EMP // gives number of rows
+          select depart ,No_Of_Employee=COUNT(Depart) from Employee group by depart //  gives the Department name as well as it's count
+          
